f61a7091a65f6755295f92d4ca48e7dcbcb403ec
pnaessen <pnaessen@student.42lyon.fr>
2025-12-05 16:29:53 +0100

    Remove unused main and user model files

--- user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User42 struct {
-	Id        int    `json:"id"`
-	Username    string `json:"login"`
-	Email       string `json:"email"`
-	School_year string `json:"pool_year"`
-	Active      bool   `json:"active?"`
-}
